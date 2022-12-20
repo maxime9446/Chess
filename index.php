c8e10124e48e1a749fc875beab4901b12afeaf22
Max Becker <beckermaxime84@gmail.com>
2022-12-20 19:32:05 +0100

    utilisation du namespace

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
+namespace App;
 require_once 'autoloader.php';
 
+
 // Creation d'une pièce + test exception < 9
-$piece = new App\Pion('A9');
+$piece = new Pion('A9');
 // permettant de savoir où est la pièce
 $position = $piece->position;
 // permettant de montrer les déplacements possibles de la pièce
@@ -12,38 +14,38 @@ $move = $piece->getPossibleLocation($position);
 $piece->show($position, $move);
 
 // test exception >0
-$piece = new App\Pion('A0');
+$piece = new Pion('A0');
 $position = $piece->position;
 $move = $piece->getPossibleLocation($position);
 $piece->show($position, $move);
 
-$piece = new App\Pion('A2');
+$piece = new Pion('A2');
 $position = $piece->position;
 $move = $piece->getPossibleLocation($position);
 $piece->show($position, $move);
 
 
-$piece = new App\Tour('B4');
+$piece = new Tour('B4');
 $position = $piece->position;
 $move = @$piece->getPossibleLocation($position);
 $piece->show($position, $move);
 
-$piece = new App\Cavalier('E5');
+$piece = new Cavalier('E5');
 $position = $piece->position;
 $move = @$piece->getPossibleLocation($position);
 $piece->show($position, $move);
 
-$piece = new App\Fou('G4');
+$piece = new Fou('G4');
 $position = $piece->position;
 $move = @$piece->getPossibleLocation($position);
 $piece->show($position, $move);
 
-$piece = new App\Reine('B8');
+$piece = new Reine('B8');
 $position = $piece->position;
 $move = @$piece->getPossibleLocation($position);
 $piece->show($position, $move);
 
-$piece = new App\Roi('C7');
+$piece = new Roi('C7');
 $position = $piece->position;
 $move = $piece->getPossibleLocation($position);
 $piece->show($position, $move);
